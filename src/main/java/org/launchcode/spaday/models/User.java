e062fdb4f22cbbae57eec8c07b604b82772fd0bc
Brittney Epperson <beppers87@hotmail.com>
2023-10-08 16:49:23 -0500

    Practicing studio activity

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,15 +1,29 @@
 package org.launchcode.spaday.models;
 
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotNull(message = "Username is required.")
+    @Size(min = 5, max = 15, message = "Username must be between 5 and 15 characters.")
     private String username;
+
+    @Email(message = "Email must be properly formatted.")
     private String email;
+
+    @NotNull(message = "Password is required.")
+    @Size(min = 6, message = "Password must be at least 6 characters long.")
     private String password;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, @RequestParam(required = false) String email, String password) {
         this();
         this.username = username;
         this.email = email;
